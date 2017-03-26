318d628edcc8dc3c17be798d8aab9ff047f17166
nedim-hasovic <hasovicnedim@gmail.com>
2017-03-26 10:04:07 +0200

    Add files via upload

---
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -0,0 +1,32 @@
+//
+//  FoodTrackerTests.swift
+//  FoodTrackerTests
+//
+//  Created by Nedim Hasovic on 16/06/16.
+//  Copyright © 2016 Nedim Hasovic. All rights reserved.
+//
+
+import UIKit
+import XCTest
+@testable import FoodTracker
+
+class FoodTrackerTests: XCTestCase {
+    
+   // MARK: FoodTracker Tests
+    
+    // Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
+    func testMealInitialization() {
+        
+        // Success case.
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
+        
+        // Failure cases.
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNotNil(badRating, "Negative ratings are invalid, be positive")
+    }
+    
+}
